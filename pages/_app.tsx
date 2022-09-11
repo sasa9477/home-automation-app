c90cf44535ca4eccdf9194ea1b5437529acd4900
sasa9477 <sasa9477@outlook.com>
2022-09-11 18:15:05 +0900

    update favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = createTheme({
+    palette: {
+      mode: isDarkMode ? 'dark' : 'light'
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
