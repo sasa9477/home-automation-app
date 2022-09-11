4f381764a1cc88f3aa7114b2d491e70b0adbb001
sasa9477 <sasa9477@outlook.com>
2022-09-12 04:21:09 +0900

    Add LogPage

---
+++ src/pages/log.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import { Stack } from '@mui/material';
+
+const LogPage: NextPage = () => {
+  return (
+    <Stack>
+    </Stack>
+  )
+}
+
+export default LogPage
