43eae6350f901c734c4fc0d8f496dc7c5220879a
sasa9477 <sasa9477@outlook.com>
2022-09-15 03:19:03 +0900

    Revert "設定画面をレスポンシブ対応"
    This reverts commit 2047da4fc8a6e97cf551863b85db073a1db20b75.

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,43 +1,35 @@
 import type { NextPage } from 'next'
-import { Grid } from '@mui/material';
+import { Stack } from '@mui/material';
 import FunctionSwitchSettingCard from '../components/FunctionSwitchSettingCard';
 
 const SettingPage: NextPage = () => {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={12} md={3}>
-        <FunctionSwitchSettingCard
-          id={1}
-          name={"hoge"}
-          ipaddress={"192.168.0.100"}
-          enabled={true}
-        />
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <FunctionSwitchSettingCard
-          id={2}
-          name={"hoge"}
-          ipaddress={"192.168.0.100"}
-          enabled={false}
-        />
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <FunctionSwitchSettingCard
-          id={3}
-          name={"hoge"}
-          ipaddress={"192.168.0.100"}
-          enabled={false}
-        />
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <FunctionSwitchSettingCard
-          id={4}
-          name={"hoge"}
-          ipaddress={"192.168.0.100"}
-          enabled={false}
-        />
-      </Grid>
-    </Grid>
+    <Stack>
+      <FunctionSwitchSettingCard
+        id={1}
+        name={"hoge"}
+        ipaddress={"192.168.0.100"}
+        enabled={true}
+      />
+      <FunctionSwitchSettingCard
+        id={2}
+        name={"hoge"}
+        ipaddress={"192.168.0.100"}
+        enabled={false}
+      />
+      <FunctionSwitchSettingCard
+        id={3}
+        name={"hoge"}
+        ipaddress={"192.168.0.100"}
+        enabled={false}
+      />
+      <FunctionSwitchSettingCard
+        id={4}
+        name={"hoge"}
+        ipaddress={"192.168.0.100"}
+        enabled={false}
+      />
+    </Stack>
   )
 }
 
