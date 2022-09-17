242484a85b09ed959f3de724fd676b0b5f0866e6
sasa9477 <sasa9477@outlook.com>
2022-09-17 16:50:19 +0900

    prsima seeding にリトライ処理を追加

--- script/prisma-seed.js
+++ script/prisma-seed.js
@@ -1,45 +1,40 @@
 // @ts-check
-
+const readline = require('readline');
 const { PrismaClient } = require('@prisma/client');
+const colors = require('colors');
 
 const prisma = new PrismaClient();
 
-/** @type {import('@prisma/client').Switcher[]} */
+/** @type {import('@prisma/client').Prisma.SwitcherCreateInput[]} */
 const switchers = [
   {
-    id: 1,
     name: 'hoge',
     ipaddress: '192.168.1.1',
-    enabled: true,
-    turnOn: false,
   },
   {
-    id: 2,
     name: 'fuga',
     ipaddress: '192.168.1.2',
     enabled: false,
-    turnOn: false,
   },
   {
-    id: 3,
     name: 'piyo',
     ipaddress: '192.168.1.3',
-    enabled: true,
-    turnOn: false,
   },
   {
-    id: 4,
     name: 'zoon',
     ipaddress: '192.168.1.4',
-    enabled: true,
-    turnOn: false,
   },
   {
-    id: 5,
     name: 'hyou',
     ipaddress: '192.168.1.5',
-    enabled: true,
-    turnOn: false,
+    enabled: false,
+  },
+];
+
+/** @type {import('@prisma/client').Prisma.LogCreateInput[]} */
+const logs = [
+  {
+    message: 'this is first log',
   },
 ];
 
@@ -51,14 +46,85 @@ const transfer = async () => {
     });
     seeds.push(seed);
   }
+  for (const log of logs) {
+    const seed = prisma.log.create({
+      data: { ...log },
+    });
+    seeds.push(seed);
+  }
+
   return await prisma.$transaction(seeds);
+};
+
+// // TemplateStringArray => https://www.prisma.io/docs/concepts/components/prisma-client/raw-database-access#using-variables
+// // https://zenn.dev/cohky/articles/prisma-to-truncate
+// const truncateAllTables = async () => {
+//   const transactions = [];
+//   // 外部キーを無効化 ※ここでエラーになるのでコメントアウト
+//   transactions.push(prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 0;`);
+
+//   const allProperties = Object.keys(prisma);
+//   const modelNames = allProperties.filter((x) => !(typeof x === 'string' && (x.startsWith('$') || x.startsWith('_'))));
+
+//   for (const modelName of modelNames) {
+//     transactions.push(prisma[modelName].deleteMany());
+//   }
+
+//   // 外部キーを有効化
+//   transactions.push(prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 1;`);
+
+//   await prisma.$transaction(transactions);
+// };
+
+const deleteAllData = async () => {
+  const modelNames = Object.keys(prisma).filter(
+    (x) => !(typeof x === 'string' && (x.startsWith('$') || x.startsWith('_')))
+  );
+
+  const queries = [];
+
+  for (const modelName of modelNames) {
+    const query = prisma[modelName].deleteMany();
+    queries.push(query);
+  }
+
+  return await prisma.$transaction(queries);
+};
+
+/** @type {(question: string) => Promise<string>} */
+const prompt = (question) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+      rl.close();
+    });
+  });
 };
 
 const main = async () => {
   console.log('Start seeding.');
 
-  const result = await transfer();
-  console.log(`Registerd ${result.length}columns.`);
+  try {
+    const result = await transfer();
+    console.log(colors.green(`Created ${result.length}columns.`));
+  } catch (e) {
+    console.error(colors.red('An error occurred.'));
+    console.error(colors.red(e.message));
+
+    const answer = await prompt(colors.yellow('Delete all data and try seeding again? (y/n) '));
+    if (answer.trim().toLowerCase() === 'y') {
+      const deleteAllDataResult = await deleteAllData();
+      const deleteCount = deleteAllDataResult.flatMap((table) => table.count).reduce((sum, count) => (sum += count));
+      console.log(colors.red(`Delete ${deleteCount} columns.`));
+
+      const result = await transfer();
+      console.log(colors.green(`Created ${result.length} columns.`));
+    }
+  }
 
   console.log('Seeding finished.');
 };
