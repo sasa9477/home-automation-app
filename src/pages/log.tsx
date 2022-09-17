37a45b12691c079eda94362d65bc55e56d899e11
sasa9477 <sasa9477@outlook.com>
2022-09-18 08:03:51 +0900

    ログページを実装

--- src/pages/log.tsx
+++ src/pages/log.tsx
@@ -1,9 +1,59 @@
-import type { NextPage } from 'next'
-import { Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import { Log } from '@prisma/client';
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
 
-const LogPage: NextPage = () => {
+import { prismaClient } from '../utils/prismaClient';
+
+import type { GetServerSideProps, NextPage } from 'next'
+
+type LogViewModel = Omit<Log, 'createdAt'> & {
+  createdAt: string
+}
+
+type LogPageProps = {
+  logs: LogViewModel[]
+}
+
+export const getServerSideProps: GetServerSideProps<LogPageProps> = async (context) => {
+  const data = await prismaClient.log.findMany({
+    orderBy: {
+      createdAt: 'asc'
+    }
+  })
+
+  const logs: LogViewModel[] = data.map(item => {
+    return {
+      ...item,
+      createdAt: format(item.createdAt, 'yyyy/MM/d HH:mm:ss', { locale: ja })
+    }
+  })
+
+  return {
+    props: {
+      logs
+    }
+  }
+}
+
+const LogPage: NextPage<LogPageProps> = ({ logs }) => {
   return (
-    <Stack>
+    <Stack sx={{ m: 1 }}>
+      {logs.map(log => (
+        <Box key={log.id}>
+          <Typography variant='caption'>
+            {log.createdAt}
+          </Typography>
+          <Typography>
+            {log.message}
+          </Typography>
+        </Box>
+      ))}
+      {logs.length === 0 &&
+        <Typography sx={{ alignSelf: 'center' }}>
+          データがありません
+        </Typography>
+      }
     </Stack>
   )
 }
