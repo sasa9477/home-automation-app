f6ccc27f154d5f9d5cbd553c64dc9a99e925d568
sasa9477 <sasa9477@outlook.com>
2022-09-18 09:32:21 +0900

    名前に一意制約を追加

--- prisma/migrations/20220918003043_initial/migration.sql
+++ prisma/migrations/20220918003043_initial/migration.sql
@@ -3,7 +3,10 @@ CREATE TABLE "Switcher" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "ipaddress" TEXT NOT NULL,
-    "enabled" BOOLEAN NOT NULL DEFAULT false
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "turnOn" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
 );
 
 -- CreateTable
@@ -12,6 +15,9 @@ CREATE TABLE "Log" (
     "message" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Switcher_name_key" ON "Switcher"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Switcher_ipaddress_key" ON "Switcher"("ipaddress");
