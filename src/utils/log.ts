211ffd5ca0b3d6be068c59b4df00ead5db7674af
sasa9477 <sasa9477@outlook.com>
2022-09-18 21:13:37 +0900

    ログ登録のユーティリティを追加

---
+++ src/utils/log.ts
@@ -0,0 +1,75 @@
+import type { PrismaClient } from '@prisma/client';
+
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
+const getCircularReplacer = (() => {
+  const seen = new WeakSet();
+  return (_: string, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+})();
+
+function messageWithObject(message: string, obj: any) {
+  const objString = JSON.stringify(obj, getCircularReplacer);
+  return message + `\n${objString}`;
+}
+
+export enum LogLevel {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+}
+
+export function logDebug(prisma: PrismaClient, message: string, paramObject?: object) {
+  if (arguments.length == 2) {
+    message = messageWithObject(message, paramObject);
+  }
+  prisma.log.create({
+    data: {
+      logLevel: LogLevel.DEBUG,
+      message,
+    },
+  });
+}
+
+export function logInformation(prisma: PrismaClient, message: string, paramObject?: object) {
+  if (arguments.length == 2) {
+    message = messageWithObject(message, paramObject);
+  }
+  prisma.log.create({
+    data: {
+      logLevel: LogLevel.INFO,
+      message,
+    },
+  });
+}
+
+export function logWarning(prisma: PrismaClient, message: string, paramObject?: object) {
+  if (arguments.length == 2) {
+    message = messageWithObject(message, paramObject);
+  }
+  prisma.log.create({
+    data: {
+      logLevel: LogLevel.WARN,
+      message,
+    },
+  });
+}
+
+export function logError(prisma: PrismaClient, message: string, paramObject?: object) {
+  if (arguments.length == 2) {
+    message = messageWithObject(message, paramObject);
+  }
+  prisma.log.create({
+    data: {
+      logLevel: LogLevel.ERROR,
+      message,
+    },
+  });
+}
