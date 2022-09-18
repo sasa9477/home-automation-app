9a6b64e8f18b4a4cff9a05112bf0a475fd5094e0
sasa9477 <sasa9477@outlook.com>
2022-09-18 21:13:15 +0900

    ログ画面にログレベルを表示

--- src/pages/log.tsx
+++ src/pages/log.tsx
@@ -3,19 +3,25 @@ import { Log } from '@prisma/client';
 import { format } from 'date-fns';
 import { ja } from 'date-fns/locale';
 
+import { LogLevel } from '../utils/log';
 import { prismaClient } from '../utils/prismaClient';
 
 import type { GetServerSideProps, NextPage } from 'next'
 
-type LogViewModel = Omit<Log, 'createdAt'> & {
-  createdAt: string
+type LogViewModel = Omit<Log, 'logLevel' | 'createdAt'> & {
+  logLevel: string,
+  createdAt: string,
+  logColor: string
 }
 
 type LogPageProps = {
   logs: LogViewModel[]
 }
 
+const logColors = ['', 'info.main', 'warning.main', 'error.main']
+
 export const getServerSideProps: GetServerSideProps<LogPageProps> = async (context) => {
+
   const data = await prismaClient.log.findMany({
     orderBy: {
       createdAt: 'asc'
@@ -25,7 +31,9 @@ export const getServerSideProps: GetServerSideProps<LogPageProps> = async (conte
   const logs: LogViewModel[] = data.map(item => {
     return {
       ...item,
-      createdAt: format(item.createdAt, 'yyyy/MM/d HH:mm:ss', { locale: ja })
+      createdAt: format(item.createdAt, 'yyyy/MM/d HH:mm:ss', { locale: ja }),
+      logLevel: LogLevel[item.logLevel],
+      logColor: logColors[item.logLevel]
     }
   })
 
@@ -40,14 +48,18 @@ const LogPage: NextPage<LogPageProps> = ({ logs }) => {
   return (
     <Stack sx={{ m: 1 }}>
       {logs.map(log => (
-        <Box key={log.id}>
-          <Typography variant='caption'>
-            {log.createdAt}
-          </Typography>
-          <Typography>
+        <Typography key={log.id}>
+          <Box component={'span'} sx={{ color: 'text.secondary' }}>
+            {log.createdAt + ' '}
+          </Box>
+          <Box component={'span'} sx={{ color: `${log.logColor}` }}>
+            {log.logLevel + ' '}
+          </Box>
+          <Box component='br' sx={{ display: { xs: undefined, sm: 'none' } }} />
+          <span>
             {log.message}
-          </Typography>
-        </Box>
+          </span>
+        </Typography>
       ))}
       {logs.length === 0 &&
         <Typography sx={{ alignSelf: 'center' }}>
